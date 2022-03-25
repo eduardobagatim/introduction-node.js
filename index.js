81db0dd737bb7b08607646de3e803f265174cb70
Eduardo Bagatim <bagatimeduardo@gmail.com>
2022-03-25 17:31:20 -0300

    conf e criação da api

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const userRoute = require('./routes/userRoute')
+
+const app = express()
+const port = 3000
+
+userRoute(app)
+
+app.get('/', (req, res) => res.send('Ola mundo pelo express'))
+
+app.listen(port, () => console.log('api rodando porta 3000'))
